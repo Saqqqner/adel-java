1d4e095212ba29aa53aee9e473e9432a9c12863c
Saqqqner <rikardofik@gmail.com>
2022-12-05 22:46:37 +0300

    ex1

---
+++ src/example1/Example1.java
@@ -0,0 +1,11 @@
+package example1;
+
+public class Example1 {
+    public static void main(String[] args) {
+        test1();
+
+    }
+    private static void test1(){
+        System.out.println("Less");
+    }
+}
